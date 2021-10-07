62e25f64e12f3d8be43747830312d13969719187
luanafreitas <luanaluciele0@gmail.com>
2021-10-07 10:34:26 -0300

    tab bar e background

--- App.js
+++ App.js
@@ -2,19 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppTab from './src/AppTab';
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
 
 function App() {
   return ( 
     <>
-      <NavigationContainer>
+    <View style={styles.container}>
+    <ImageBackground  
+    source={require('./assets/fundo_provisorio.jpg')} 
+    style={styles.image} >
 
-      <AppTab/>
+
+
       
-      </NavigationContainer>
+      <AppTab />
+
+     </ImageBackground> 
+     </View>
     </>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    resizeMode: 'cover',
+    
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
+});
 
 export default App;
--- src/AppTab.js
+++ src/AppTab.js
@@ -2,37 +2,101 @@ import React from 'react';
 
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabBarHeightContext } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { BlurView } from 'expo-blur' ;
 
 import AppList from './AppList';
 import AppForm from './AppForm';
 
 const Tab = createBottomTabNavigator();
+const theme = { //like this
+    colors: {
+      background: "transparent",
+    },
+  };
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 25,
+        left: 20,
+        right: 20,
+        elevation: 0,
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        height: 40,
+        ...style.shadow,
+      }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityStates={isFocused ? ['selected'] : []}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1, alignItems: 'center' }}>
+            <Text style={{ color: isFocused ? '#0077FF' : '#0077FF' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
 
 function AppTab() {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      tabBarOptions={{
-        showLabel: false,
-        
-        style: {
-          position: 'absolute',
-          bottom: 25,
-          left: 20,
-          right: 20,
-          elevation: 0,
-          backgroundColor: '#fff',
-          borderRadius: 15,
-          height: 90,
-          ...style.shadow,
-        },
-        
-      }}>
-      <Tab.Screen name="Home" component={AppForm} />
-      <Tab.Screen name="List" component={AppList} />
-    </Tab.Navigator>
+    <NavigationContainer theme={theme}>
+      <Tab.Navigator
+        tabBar={(props) => <MyTabBar {...props} />}
+        screenOptions={{
+          headerShown: false,
+        }}
+        tabBarOptions={{
+          showLabel: false,
+        }}
+        >
+        <Tab.Screen name="Home" component={AppForm} />
+        <Tab.Screen name="List" component={AppList} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -46,16 +110,6 @@ const style = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.5,
     elevation: 5,
-  },
-  teste: {
-    position: 'absolute',
-    bottom: 25,
-    left: 20,
-    right: 20,
-    elevation: 0,
-    backgroundColor: '#fff',
-    borderRadius: 15,
-    height: 90,
   },
 });
 
