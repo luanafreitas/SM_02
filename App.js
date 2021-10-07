0b5f9bb895174a09165e3f72e73cd20b811f9279
luanafreitas <luanaluciele0@gmail.com>
2021-10-06 22:58:06 -0300

    resolvendo app.js

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
 import AppTab from './src/AppTab';
 
-export default function App() {
+
+
+function App() {
   return ( 
     <>
-      <AppTab />
-      <StatusBar style="light" />
+      <NavigationContainer>
+
+      <AppTab/>
+      
+      </NavigationContainer>
     </>
   );
 }
+
+export default App;
